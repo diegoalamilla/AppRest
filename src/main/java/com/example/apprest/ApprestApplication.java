6d68344ca20d8a3a7f40d1bf394bb65e2037fa2b
diegoalamilla <diego.alamilla123@gmail.com>
2024-11-02 18:30:58 -0600

    home page update

--- src/main/java/com/example/apprest/ApprestApplication.java
+++ src/main/java/com/example/apprest/ApprestApplication.java
@@ -3,13 +3,15 @@ package com.example.apprest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApprestApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Ingresa tus datos gooo";
+		return "Haz tus peticiones";
 	}
 
 	public static void main(String[] args) {
