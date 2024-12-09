0d6f3eb2f9404f08dcfc93f1ae1625f9f308055f
diegoalamilla <diego.alamilla123@gmail.com>
2024-12-09 10:46:30 -0600

    removed unused import

--- src/main/java/com/example/apprest/controllers/AlumnoController.java
+++ src/main/java/com/example/apprest/controllers/AlumnoController.java
@@ -11,7 +11,6 @@ import com.password4j.Password;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
